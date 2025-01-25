422db79c08e8c529274d4b48d220c2b2a0531813
Moises Alvarez <mfalvare@espol.edu.ec>
2025-01-24 19:02:22 -0500

    anadiendo imagen login

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import Login from '@react-login-page/page11';
+import LoginBannerBgImg from '../img/meeting.png';
 
-const Demo = () => {
-  const [data, setData] = React.useState({});
-  const handle = (even) => {
-    even.preventDefault();
-    const formData = new FormData(even.target);
-    const data = Object.fromEntries(formData);
-    setData({ ...data });
-  };
-  return (
-    <form onSubmit={handle}>
-      <Login style={{ minHeight: 380 }} />
-      <h3>Result:</h3>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </form>
-  );
-};
+const Demo = () => (
+  <Login style={{ height: 380 }}>
+    <Login.Banner>
+      <img src={LoginBannerBgImg} alt="banner" />
+    </Login.Banner>
+  </Login>
+);
 
 export default Demo;
